f5429ba179e6b87cae5e6537350609effdf33f5c
Todor Dimitrov <todor.dimitrov@stud.uni-due.de>
2011-08-01 18:16:04 +0200

    Initial Commit

---
+++ OntologyTester/OntologyTesterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  OntologyTesterAppDelegate.h
+//  OntologyTester
+//
+//  Created by Todor Dimitrov on 8/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OntologyTesterAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ OntologyTester/OntologyTesterAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  OntologyTesterAppDelegate.m
+//  OntologyTester
+//
+//  Created by Todor Dimitrov on 8/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OntologyTesterAppDelegate.h"
+
+@implementation OntologyTesterAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
